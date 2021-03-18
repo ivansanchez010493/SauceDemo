5b845d11ecf2daf9bdf90bcbbee67800975f8cd6
ivansanchez010493 <ivansanchez.010493@gmail.com>
2021-03-18 00:26:51 -0600

    Test case to add multiple products was added

--- page_model/pages/ShoppingCartPage.js
+++ page_model/pages/ShoppingCartPage.js
@@ -3,6 +3,11 @@ import {Selector, t} from 'testcafe'
 class ShoppingCartPage{
     constructor(){
         this.lblCartTitle = Selector('div.subheader')
+        this.lblProductName = Selector('.inventory_item_name')
+    }
+
+    async validateProduct(productName){
+        await t.expect(this.lblProductName.withText(productName).exists).ok()
     }
   }
   
--- page_model/test/ShoppingCart.test.js
+++ page_model/test/ShoppingCart.test.js
@@ -3,6 +3,8 @@ import { CREDENTIALS } from '../data/Constants'
 import ProductsPage from '../pages/ProductsPage'
 import ShoppingCartPage from '../pages/ShoppingCartPage'
 
+const products = require('../data/products.json');
+
 fixture('Feature Shopping Cart')
   .page `https://www.saucedemo.com/`
   
@@ -15,4 +17,17 @@ test('As a registered user can enter to shopping cart section', async t => {
 test('As a registered user can add a product to the cart', async t => {
     await LoginPage.submitLoginForm(CREDENTIALS.VALID_USER.USERNAME, CREDENTIALS.VALID_USER.PASSWORD)
     await ProductsPage.addProduct('Sauce Labs Backpack')
+    await t.click(ProductsPage.btnShoppingCart)
+    await ShoppingCartPage.validateProduct('Sauce Labs Backpack')
+})
+
+test('As a registered user can add multiple products to the cart', async t => {
+  await LoginPage.submitLoginForm(CREDENTIALS.VALID_USER.USERNAME, CREDENTIALS.VALID_USER.PASSWORD)
+  for(const { name } of products) {
+    await ProductsPage.addProduct(name)
+  }
+  await t.click(ProductsPage.btnShoppingCart)
+  for(const { name } of products) {
+   await ShoppingCartPage.validateProduct(name)
+  }
 })
